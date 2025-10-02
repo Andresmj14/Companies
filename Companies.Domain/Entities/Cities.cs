05a74584580d0f9eb93cd15796add10e6a9f23f1
Andresmj14 <andresmjs11@gmail.com>
2025-10-02 00:14:04 -0500

    Update Cities.cs

--- Companies.Domain/Entities/Cities.cs
+++ Companies.Domain/Entities/Cities.cs
@@ -4,31 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Http.Headers;
 
-namespace Companies.Domain.Entities
+namespace Companies.Domain.Entities;
+
+public class Cities
 {
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
 
-    public class Cities
+    public int RegionId { get; set; }
+    public virtual Regions? Region { get; set; }
+
+    public ICollection<Companies> Companies { get; set; } = new List<Companies>();
+    public ICollection<Branches> Branches { get; set; } = new List<Branches>();
+
+    public Cities() { }
+    public Cities(string name, int regionId)
     {
-        public int id { get; set; } = Guid.NewGuid().GetHashCode();
-        public string name { get; set; } = null!;
-        public int Regionsid {get; set; }
-        public Regions Regions { get; set; } = null!;
-        public List<Companie> companiesid { get; set; } = new List<Companie>();
-        public List<Branches> branchesid { get; set; } = new List<Branches>();
-
-
-        protected Cities() { }
-        public Cities(string Name)
-        {
-            this.name = Name;
-        }
-        public void UpdateName(string newName)
-        {
-            name = newName;
-        }
+        Name = name;
+        RegionId = regionId;
     }
-    
-
-
-
 }
