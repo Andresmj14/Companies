7a7e8ba3ad352e34741ea4f0ebeca51859477245
Andresmj14 <andresmjs11@gmail.com>
2025-10-02 00:15:13 -0500

    Update Regions.cs

--- Companies.Domain/Entities/Regions.cs
+++ Companies.Domain/Entities/Regions.cs
@@ -5,23 +5,21 @@ using System.Threading.Tasks;
 using System.Net.Http.Headers;
 
 
-namespace Companies.Domain.Entities
+namespace Companies.Domain.Entities;
+public class Regions
 {
-    public class Regions
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+
+    public int CountryId { get; set; }
+    public virtual Countries? Country { get; set; }
+
+    public ICollection<Cities> Cities { get; set; } = new List<Cities>();
+
+    public Regions() { }
+    public Regions(string name, int countryId)
     {
-        public int id { get; set; } = Guid.NewGuid().GetHashCode();
-        public string name { get; set; } = null!;
-        public int Countriesid {get; set; }
-        public Countries Countries { get; set; } = null!;
-        public List<Cities> citiesid { get; set; } = new List<Cities>();
-        protected Regions() { }
-        public Regions(string name)
-        {
-            this.name = name;
-        }
-        public void UpdateName(string newName)
-        {
-            name = newName;
-        }
+        Name = name;
+        CountryId = countryId;
     }
 }
